fd70c54a14847f88a77978b03ff4c5d7f10e7322
zerocheducat <2463836858@qq.com>
2024-11-21 17:37:19 +0800

    Site updated: 2024-11-21 17:37:18

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732178968256";
+const VERSION = "1732181835657";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
